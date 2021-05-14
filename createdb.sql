d07a2f4789cc555f3089e3ec531034205b3ec2da
Finn Jaekel <s6fijaek@uni-bonn.de>
2021-05-14 17:11:04 +0200

    Adding Local Numbering into Database

--- createdb.sql
+++ createdb.sql
@@ -48,13 +48,13 @@ llog text,
 PRIMARY KEY(Measurement_ID)
 );
 
-CREATE TABLE PaketID(
+CREATE TABLE PacketID(
 Hybrid_ID text,
-Paket_ID text,
+LocalNumbering text,
 Comments text,
 PRIMARY KEY(Hybrid_ID)
 );
 
-CREATE VIEW Hybrid_Overview_view as SELECT Hybrid_Overview.Hybrid_ID,Hybrid_Overview.Measurement_ID, Paket_ID, Hybrid_Overview.Hybrid_Class, Hybrid_Overview.VMM, Hybrid_Overview.VMM_Class, Hybrid_Overview.BrokenChannels, Hybrid_Overview.Location, Hybrid_Overview.Date, Comments, Hybrid_Overview.MonitorADCSlope, Hybrid_Overview.MonitorADCIntercept, Hybrid_Overview.Baseline, Hybrid_Overview.BaseNoise, Hybrid_Overview.Threshold, Hybrid_Overview.WorkingChannelsExt, Hybrid_Overview.WorkingChannelsInt, Hybrid_Overview.AvgCurr1_9V, Hybrid_Overview.AvgCurr2_9V, Hybrid_Overview.ADCExtCurvature, Hybrid_Overview.ADCIntCurvature, Hybrid_Overview.ADCExtVertex, Hybrid_Overview.ADCIntVertex, Hybrid_Overview.AVGTDC FROM Hybrid_Overview LEFT JOIN PaketID ON Hybrid_Overview.Hybrid_ID= PaketID.Hybrid_ID;
+CREATE VIEW Hybrid_Overview_view as SELECT Hybrid_Overview.Hybrid_ID,Hybrid_Overview.Measurement_ID, LocalNumbering, Hybrid_Overview.Hybrid_Class, Hybrid_Overview.VMM, Hybrid_Overview.VMM_Class, Hybrid_Overview.BrokenChannels, Hybrid_Overview.Location, Hybrid_Overview.Date, Comments, Hybrid_Overview.MonitorADCSlope, Hybrid_Overview.MonitorADCIntercept, Hybrid_Overview.Baseline, Hybrid_Overview.BaseNoise, Hybrid_Overview.Threshold, Hybrid_Overview.WorkingChannelsExt, Hybrid_Overview.WorkingChannelsInt, Hybrid_Overview.AvgCurr1_9V, Hybrid_Overview.AvgCurr2_9V, Hybrid_Overview.ADCExtCurvature, Hybrid_Overview.ADCIntCurvature, Hybrid_Overview.ADCExtVertex, Hybrid_Overview.ADCIntVertex, Hybrid_Overview.AVGTDC FROM Hybrid_Overview LEFT JOIN PacketID ON Hybrid_Overview.Hybrid_ID= PacketID.Hybrid_ID;
 
 CREATE VIEW latest_measurements as SELECT * from hybrid_overview_view WHERE measurement_id IN (SELECT max FROM(SELECT hybrid_id,MAX(measurement_id) as max FROM hybrid_overview GROUP BY hybrid_id) as foo);
