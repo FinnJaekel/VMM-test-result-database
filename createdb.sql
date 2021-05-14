ff99e4738c16728ee0ccb8403311384187783e36
Finn Jaekel <s6fijaek@uni-bonn.de>
2021-05-14 16:34:56 +0200

    Trying Changes in DB Structure

--- createdb.sql
+++ createdb.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Hybrid_Overview(
+CREATE TABLE Hybrid_Information(
 Hybrid_ID text,
 Measurement_ID text,
 Hybrid_Class text,
@@ -54,5 +54,7 @@ Paket_ID text,
 Comments text,
 PRIMARY KEY(Hybrid_ID)
 );
+
+CREATE VIEW Hybrid_Overview as SELECT Hybrid_Overview.Hybrid_ID,Hybrid_Overview.Measurement_ID, Paket_ID, Hybrid_Overview.Hybrid_Class, Hybrid_Overview.VMM, Hybrid_Overview.VMM_Class, Hybrid_Overview.BrokenChannels, Hybrid_Overview.Location, Hybrid_Overview.Date, Comments, Hybrid_Overview.MonitorADCSlope, Hybrid_Overview.MonitorADCIntercept, Hybrid_Overview.Baseline, Hybrid_Overview.BaseNoise, Hybrid_Overview.Threshold, Hybrid_Overview.WorkingChannelsExt, Hybrid_Overview.WorkingChannelsInt, Hybrid_Overview.AvgCurr1_9V, Hybrid_Overview.AvgCurr2_9V, Hybrid_Overview.ADCExtCurvature, Hybrid_Overview.ADCIntCurvature, Hybrid_Overview.ADCExtVertex, Hybrid_Overview.ADCIntVertex, Hybrid_Overview.AVGTDC FROM Hybrid_Overview LEFT JOIN PaketID ON Hybrid_Overview.Hybrid_ID= PaketID.Hybrid_ID;
 
 CREATE VIEW latest_measurements as SELECT * from hybrid_overview WHERE measurement_id IN (SELECT max FROM(SELECT hybrid_id,MAX(measurement_id) as max FROM hybrid_overview GROUP BY hybrid_id) as foo);
